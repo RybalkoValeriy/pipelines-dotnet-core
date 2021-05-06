d6e168c0fb2945024f34e8632e00c4e282bbc154
Valerii Rybalko <Valerii.Rybalko@enghouse.com>
2021-05-06 11:57:08 +0300

    upd 2

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace pipelines_dotnet_core
             CreateWebHostBuilder(args).Build().Run();
         }
 
-// some update in develop
+// some update in develop 2
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
